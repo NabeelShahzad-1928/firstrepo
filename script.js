27e73b51bf782608ffe624f0e15c7a10f9866976
NabeelShahzad-1928 <nabeelcohan34@gmail.com>
2023-05-09 11:47:35 +0500

    first comit

---
+++ script.js
@@ -0,0 +1,48 @@
+// var heading2 = document.getElementById("heading1");
+//  heading2.innerHTML = "<h1>This is heading 2</h1>";
+//  console.log(heading2);
+
+// function myName(){
+// document.getElementById("heading2").style.display="block"
+// }
+// function finsName(){
+// document.getElementById("heading2").style.display="none"
+// 
+
+// var valu = prompt("what you cloth,bike,shoes");
+// var Heading = document.getElementById("heading").innerText= valu;
+// if(valu=="cloth"){
+//     var clothes= document.getElementById("List");
+//     clothes.innerHTML = "<li>Mens cloth</li> <li>Baby cloth</li> <li>pent,shirt</li> <li>females cloth</li> <li>pent cout</li>";
+// }
+// else if(valu=="bike"){
+//     var Bikes= document.getElementById("List1");
+//     Bikes.innerHTML = "<li>Mens bikes</li> <li>Baby bikes</li> <li>Heavy bikes</li> <li>scoter</li> <li>hondas 70</li>";
+// }
+// else if(valu=="shoes"){
+//     var Shoes= document.getElementById("List2");
+//     Shoes.innerHTML = "<li>Mens shoes</li> <li>Baby shoes</li> <li>Heavy hils</li> <li>lades shoes</li> <li>chapels</li>";
+// }
+
+// flag variable
+
+
+// var students = ["nabeel", "umer", "muzammil", "azan", "mozam"];
+// var listName  = prompt("enter you student name");
+
+// var matchFound = false;
+
+// for(var i = 0 ; i < students.length; i++){
+//     if(listName.toLowerCase() === students[i]){
+//         matchFound = true;
+//         console.log("your in my class");
+//         break;
+//     }
+//    console.log (students[i]);
+// }
+// if(matchFound==false){
+//     console.log("you are not in my class");
+// }
+
+
+
